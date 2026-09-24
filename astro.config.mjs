b1162c5e17194908cc1e94c9e3b275932d6deda9
Daniel Umar <jenazahrizani@gmail.com>
2026-09-24 14:28:29 +0700

    Deploy BINSIGHT Preview

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://jenazahrizani.github.io",
+  base: "/BINSIGHT-Preview",
+  trailingSlash: "always",
+});
